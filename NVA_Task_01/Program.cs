af0ddb0780ac58c65f73d2dbed96b87dfaccedb8
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-17 19:50:51 +0300

    Добавьте файлы проекта.

---
+++ NVA_Task_01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+var rnd = new Random();
+Console.Write("Введите кол-во золота:");
+int.TryParse(Console.ReadLine(), out int gold);
+
+
+var countCrystal = rnd.Next(1, 10);
+Console.WriteLine($"Вы готовы купить {countCrystal} шт. алмазов за: ");
+var priceCrystal = Console.ReadLine();
+
+Console.WriteLine($"Сделка завершена!\nЗолота: {gold}\nКристаллов: ${10}");
+
+
