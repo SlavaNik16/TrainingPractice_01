96fc0520f3670f124719aaa01263dd53ddc893de
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-17 20:47:12 +0300

    task

--- NVA_Task_01/Program.cs
+++ NVA_Task_01/Program.cs
@@ -1,13 +1,35 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 var rnd = new Random();
 Console.Write("Введите кол-во золота:");
-int.TryParse(Console.ReadLine(), out int gold);
+int gold = 0;
 
+while(!int.TryParse(Console.ReadLine(), out gold) || gold <= 0)
+{
+    Console.WriteLine("Неправильно введенные данные");
+}
 
 var countCrystal = rnd.Next(1, 10);
-Console.WriteLine($"Вы готовы купить {countCrystal} шт. алмазов за: ");
-var priceCrystal = Console.ReadLine();
+Console.Write($"Вы готовы купить {countCrystal} шт. алмазов за: ");
+int priceCrystal = 0;
 
-Console.WriteLine($"Сделка завершена!\nЗолота: {gold}\nКристаллов: ${10}");
+while(!int.TryParse(Console.ReadLine(), out priceCrystal) || priceCrystal <= 0)
+{
+    Console.WriteLine("Неправильно введенные данные");
+}
+
+while(gold - priceCrystal > 0)
+{
+    gold-= priceCrystal;
+    Console.WriteLine($"Сделка успешно завершена!\nУ вас осталось:\nЗолота: {gold}\nКристаллов: ${countCrystal}");
+    Environment.Exit(0);
+}
+
+Console.WriteLine($"Сделка завершена неудачно!\nУ было \nЗолота: {gold}\nКристаллов: ${countCrystal}");
 
 
+
+
+
+Console.ReadKey(true);
