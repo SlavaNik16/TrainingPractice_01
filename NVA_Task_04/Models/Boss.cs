9a0994fc21adee8f55513d327efafbe1884995f4
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2023-04-18 19:32:34 +0300

    результат

---
+++ NVA_Task_04/Models/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVA_Task_04.Models
+{
+    public class Boss
+    {
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int HP { get; set; }
+        public int MP { get; set; }
+
+        public Boss(string name, int attack, int hP, int mP)
+        {
+            Name = name;
+            Attack = attack;
+            HP = hP;
+            MP = mP;
+        }
+    }
+}
